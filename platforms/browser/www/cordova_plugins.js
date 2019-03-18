5b18300a4fd9e52b91612aa274d12549507ef608
danish1023 <danish1023@gmail.com>
2019-03-18 14:39:20 +0530

    network information

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -394,6 +394,29 @@ module.exports = [
         "id": "phonegap-plugin-2mundos-barcodescanner-custom-ui.BarcodeScannerProxy",
         "pluginId": "phonegap-plugin-2mundos-barcodescanner-custom-ui",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/src/browser/network.js",
+        "id": "cordova-plugin-network-information.NetworkInfoProxy",
+        "pluginId": "cordova-plugin-network-information",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -419,7 +442,8 @@ module.exports.metadata =
     "cordova-plugin-streaming-media": "2.2.0",
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-x-toast": "2.7.0",
-    "phonegap-plugin-2mundos-barcodescanner-custom-ui": "7.0.0"
+    "phonegap-plugin-2mundos-barcodescanner-custom-ui": "7.0.0",
+    "cordova-plugin-network-information": "2.0.1"
 }
 // BOTTOM OF METADATA
 });
