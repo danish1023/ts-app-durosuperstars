a4034d40a17c26f3d9b335a25b88506235222e3d
Anurag Mittal <techwork@Anurags-Mac-mini.local>
2019-08-07 15:50:05 +0530

    app version change

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,8 @@ var $$ = Dom7;
 // Platform
 var platform = 'android';
 // API Version
-var AppVersionAndroid = '1.3';
-var AppVersionIOS = '1.0';
+var AppVersionAndroid = '1.4'; // 1.3
+var AppVersionIOS = '1.1'; // 1.0
 
 var User = localStorage.User;
 if (User) {
